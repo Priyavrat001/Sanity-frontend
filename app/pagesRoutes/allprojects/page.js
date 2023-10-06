0a391f4c2d17ee6550a4ef1f734a7dd3f0bada44
Priyavrat Kumar <122096583+Priyavrat001@users.noreply.github.com>
2023-10-06 14:48:08 +0530

    Added project data from backend

--- app/pagesRoutes/allprojects/page.js
+++ app/pagesRoutes/allprojects/page.js
@@ -1,6 +1,11 @@
-import React from "react"
+import Link from 'next/link';
+import React from 'react';
+import client, { urlFor } from '@/app/components/client';
 
-const Allproject = () => {
+const Allproject = async() => {
+  let query = `*[_type == "project"]`;
+  const data = await client.fetch(query);
+  console.log(data)
   return (
     < div >
       <h1 className="text-blue-800 text-5xl text-center my-3">Here is my all Projects</h1>
@@ -17,52 +22,45 @@ const Allproject = () => {
       </div>
       <section className="text-gray-600 body-font">
         <div className="container px-5 py-24 mx-auto">
-          <div className="flex flex-col">
-            <div className="h-1 bg-gray-200 rounded overflow-hidden">
-              <div className="w-24 h-full bg-indigo-500"></div>
-            </div>
-            <div className="flex flex-wrap sm:flex-row flex-col py-6 mb-12">
-              <h1 className="sm:w-2/5 text-gray-900 font-bold title-font text-2xl mb-2 sm:mb-0">Projects</h1>
-            </div>
-          </div>
-          <div className="flex flex-wrap sm:-m-4 -mx-4 -mb-10 -mt-4">
-            <div className="p-4 md:w-1/3 sm:mb-0 mb-6">
-              <div className="rounded-lg h-64 overflow-hidden">
-                <img alt="content" className="object-cover object-center h-full w-full" src="https://dummyimage.com/1203x503" />
+          <div className="container px-5 py-24 mx-auto">
+              <div className="flex flex-wrap -m-4">
+                {data.map((e) => (
+                  <div className="p-4 md:w-1/3" key={e._id}>
+                    <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+                      <img lazyloading="true"
+                        className="lg:h-48 md:h-36 w-full object-cover object-center"
+                        src={urlFor(e.image).url() || "/assets/img/post-01.png"}
+                        alt="blog"
+                      />
+                      <div className="p-6">
+                        <h1 className="title-font text-lg font-medium text-gray-900 mb-3">
+                          {e.projectTitle}
+                        </h1>
+                        <p className="leading-relaxed mb-3">{e.desc}</p>
+                        <div className="flex items-center flex-wrap">
+                          <Link href={e.url}  className="text-indigo-500 cursor-pointer inline-flex items-center md:mb-2 lg:mb-0">
+                            Read More
+                            <svg
+                              className="w-4 h-4 ml-2"
+                              viewBox="0 0 24 24"
+                              stroke="currentColor"
+                              strokeWidth="2"
+                              fill="none"
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                            >
+                              <path d="M5 12h14"></path>
+                              <path d="M12 5l7 7-7 7"></path>
+                            </svg>
+                          </Link>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                ))}
               </div>
-              <h2 className="text-xl font-medium title-font text-gray-900 mt-5">Shooting Stars</h2>
-              <p className="text-base leading-relaxed mt-2">Swag shoivdigoitch literally meditation subway tile tumblr cold-pressed. Gastropub street art beard dreamcatcher neutra, ethical XOXO lumbersexual.</p>
-              <a className="text-indigo-500 inline-flex items-center mt-3 hover:cursor-pointer">Live Demo
-                <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 ml-2" viewBox="0 0 24 24">
-                  <path d="M5 12h14M12 5l7 7-7 7"></path>
-                </svg>
-              </a>
-            </div>
-            <div className="p-4 md:w-1/3 sm:mb-0 mb-6">
-              <div className="rounded-lg h-64 overflow-hidden">
-                <img alt="content" className="object-cover object-center h-full w-full" src="https://dummyimage.com/1204x504" />
               </div>
-              <h2 className="text-xl font-medium title-font text-gray-900 mt-5">The Catalyzer</h2>
-              <p className="text-base leading-relaxed mt-2">Swag shoivdigoitch literally meditation subway tile tumblr cold-pressed. Gastropub street art beard dreamcatcher neutra, ethical XOXO lumbersexual.</p>
-              <a className="text-indigo-500 inline-flex items-center mt-3 hover:cursor-pointer">Live Demo
-                <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 ml-2" viewBox="0 0 24 24">
-                  <path d="M5 12h14M12 5l7 7-7 7"></path>
-                </svg>
-              </a>
-            </div>
-            <div className="p-4 md:w-1/3 sm:mb-0 mb-6">
-              <div className="rounded-lg h-64 overflow-hidden">
-                <img alt="content" className="object-cover object-center h-full w-full" src="https://dummyimage.com/1205x505" />
-              </div>
-              <h2 className="text-xl font-medium title-font text-gray-900 mt-5">The 400 Blows</h2>
-              <p className="text-base leading-relaxed mt-2">Swag shoivdigoitch literally meditation subway tile tumblr cold-pressed. Gastropub street art beard dreamcatcher neutra, ethical XOXO lumbersexual.</p>
-              <a className="text-indigo-500 inline-flex items-center mt-3 hover:cursor-pointer">Live Demo
-                <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 ml-2" viewBox="0 0 24 24">
-                  <path d="M5 12h14M12 5l7 7-7 7"></path>
-                </svg>
-              </a>
-            </div>
-          </div>
+        
         </div>
       </section>
     </div >
--- app/pagesRoutes/projects/page.js
+++ app/pagesRoutes/projects/page.js
@@ -1,6 +1,10 @@
-import React from 'react'
+import React from 'react';
+import client, { urlFor } from '@/app/components/client';
 
-const Project = () => {
+const Project = async() => {
+  let query = `*[_type == "project"]{projectTitle, desc, image, url}[0...3]`;
+  const data = await client.fetch(query);
+  console.log(data)
   return (
     <div>
         <section className="text-gray-600 body-font">
@@ -16,35 +20,11 @@ const Project = () => {
     <div className="flex flex-wrap sm:-m-4 -mx-4 -mb-10 -mt-4">
       <div className="p-4 md:w-1/3 sm:mb-0 mb-6">
         <div className="rounded-lg h-64 overflow-hidden">
-          <img alt="content" className="object-cover object-center h-full w-full" src="https://dummyimage.com/1203x503"/>
+          <img alt="content" className="object-cover object-center h-full w-full" src={data.image}/>
         </div>
-        <h2 className="text-xl font-medium title-font text-gray-900 mt-5">Shooting Stars</h2>
-        <p className="text-base leading-relaxed mt-2">Swag shoivdigoitch literally meditation subway tile tumblr cold-pressed. Gastropub street art beard dreamcatcher neutra, ethical XOXO lumbersexual.</p>
-        <a className="text-indigo-500 inline-flex items-center mt-3 hover:cursor-pointer">Live Demo
-          <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 ml-2" viewBox="0 0 24 24">
-            <path d="M5 12h14M12 5l7 7-7 7"></path>
-          </svg>
-        </a>
-      </div>
-      <div className="p-4 md:w-1/3 sm:mb-0 mb-6">
-        <div className="rounded-lg h-64 overflow-hidden">
-          <img alt="content" className="object-cover object-center h-full w-full" src="https://dummyimage.com/1204x504"/>
-        </div>
-        <h2 className="text-xl font-medium title-font text-gray-900 mt-5">The Catalyzer</h2>
-        <p className="text-base leading-relaxed mt-2">Swag shoivdigoitch literally meditation subway tile tumblr cold-pressed. Gastropub street art beard dreamcatcher neutra, ethical XOXO lumbersexual.</p>
-        <a className="text-indigo-500 inline-flex items-center mt-3 hover:cursor-pointer">Live Demo
-          <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 ml-2" viewBox="0 0 24 24">
-            <path d="M5 12h14M12 5l7 7-7 7"></path>
-          </svg>
-        </a>
-      </div>
-      <div className="p-4 md:w-1/3 sm:mb-0 mb-6">
-        <div className="rounded-lg h-64 overflow-hidden">
-          <img alt="content" className="object-cover object-center h-full w-full" src="https://dummyimage.com/1205x505"/>
-        </div>
-        <h2 className="text-xl font-medium title-font text-gray-900 mt-5">The 400 Blows</h2>
-        <p className="text-base leading-relaxed mt-2">Swag shoivdigoitch literally meditation subway tile tumblr cold-pressed. Gastropub street art beard dreamcatcher neutra, ethical XOXO lumbersexual.</p>
-        <a className="text-indigo-500 inline-flex items-center mt-3 hover:cursor-pointer">Live Demo
+        <h2 className="text-xl font-medium title-font text-gray-900 mt-5">{data.title}</h2>
+        <p className="text-base leading-relaxed mt-2">{data.desc}</p>
+        <a href={data.url} className="text-indigo-500 inline-flex items-center mt-3 hover:cursor-pointer">Live Demo
           <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 ml-2" viewBox="0 0 24 24">
             <path d="M5 12h14M12 5l7 7-7 7"></path>
           </svg>
